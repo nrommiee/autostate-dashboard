a62d3897c262ea096c1f5cb67cf2efc9c4b7a2fd
nrommiee <nrommiee@icloud.com>
2026-01-03 16:48:32 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'woaxmqckupcgwsjbnlep.supabase.co',
+        pathname: '/storage/**',
+      },
+    ],
+  },
+}
 
 module.exports = nextConfig
