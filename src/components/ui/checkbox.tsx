9300adb40f4495a7e42a28995bc755c7bf41f497
nrommiee <nrommiee@icloud.com>
2026-01-05 23:18:29 +0100

    Update checkbox.tsx

--- src/components/ui/checkbox.tsx
+++ src/components/ui/checkbox.tsx
@@ -6,7 +6,7 @@ import { Check } from "lucide-react"
 
 import { cn } from "@/lib/utils"
 
-const Checkbox = React.forwardRef
+const Checkbox = React.forwardRef<
   React.ElementRef<typeof CheckboxPrimitive.Root>,
   React.ComponentPropsWithoutRef<typeof CheckboxPrimitive.Root>
 >(({ className, ...props }, ref) => (
