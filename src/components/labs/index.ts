ef155bdf966c8620b65d544f6248632bd8b04276
nrommiee <nrommiee@icloud.com>
2026-01-10 14:21:51 +0100

    Create index.ts to export lab components

---
+++ src/components/labs/index.ts
@@ -0,0 +1,11 @@
+export { Breadcrumb, type BreadcrumbItem } from './Breadcrumb'
+export { ROIEditor, type ROIZone, type ZoneType, type IndexConfig } from './ROIEditor'
+export { PreprocessingEditor, type PreprocessingConfig } from './PreprocessingEditor'
+export { 
+  ConfigHistoryPanel, 
+  PhotoResultsDetail,
+  type TestConfig, 
+  type TestResult, 
+  type PhotoResult 
+} from './ConfigHistoryPanel'
+export { FolderTestPage } from './FolderTestPage'
